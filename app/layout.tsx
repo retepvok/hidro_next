49f6bdd1814a68b1b36e77055df946a9f25e2930
Kovács Péter <retepvok@gmail.com>
2025-03-16 18:37:57 +0100

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        {/*<meta charSet="utf-8">*/}
+        <title>Hidroworks</title>
+        {/*<base href="/">*/}
+        {/*<meta name="viewport" content="width=device-width, initial-scale=1">*/}
+        {/*<link rel="icon" type="image/x-icon" href="/assets/favicon.ico">*/}
+      </head>
       <body>{children}</body>
     </html>
   )
